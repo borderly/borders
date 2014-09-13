d5cc0b0dc0bae3d185db7045de7fb56bf3098397
Alex B <me@alexb.ninja>
2014-09-13 00:58:04 -0700

    add more tests + date

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var restify = require('restify');
+var moment = require('moment');
 
 function respond(req, res, next) {
   res.send({'hello': req.params.name});
@@ -10,11 +11,23 @@ function root(req, res, next) {
   next();
 }
 
+function date(req, res, next) {
+  res.send({'date':moment().format('MMMM Do YYYY, h:mm:ss a')});
+}
+
+function uhoh(req, res, next) {
+  res.send({'404':'content not found'})
+}
+
 var server = restify.createServer();
 server.get('/', root);
 server.head('/', root);
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
+server.get('/date', date);
+server.head('/date', date);
+server.get('/(.*)/', uhoh);
+server.head('/(.*)/', uhoh);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
--- test/test.js
+++ test/test.js
@@ -1,5 +1,29 @@
 var request = require('supertest');
+var moment = require('moment');
+
+describe('GET /', function(){
+setTimeout('500');
+  it('respond with json', function(done){
+    request('http://localhost:8080')
+      .get('/')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  })
+  it('repsond with 200 status', function(done){
+    request('http://localhost:8080')
+      .get('/')
+      .expect(200, done);
+  })
+  it('repsond with world', function(done){
+    request('http://localhost:8080')
+      .get('/')
+      .expect('"hello world"', done);
+  })
+})
+
 describe('GET /users', function(){
+setTimeout('500');
   it('respond with json', function(done){
     request('http://localhost:8080')
       .get('/hello/alex')
@@ -24,22 +48,23 @@ describe('GET /users', function(){
   })
 })
 
-describe('GET /', function(){
+describe('GET /date', function() {
+setTimeout('500');
   it('respond with json', function(done){
     request('http://localhost:8080')
-      .get('/')
+      .get('/date')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done);
   })
   it('repsond with 200 status', function(done){
     request('http://localhost:8080')
-      .get('/')
+      .get('/date')
       .expect(200, done);
   })
-  it('repsond with world', function(done){
+  it('respond with current date', function(done){
     request('http://localhost:8080')
-      .get('/')
-      .expect('"hello world"', done);
+      .get('/date')
+      .expect('{"date":"'+moment().format('MMMM Do YYYY, h:mm:ss a')+'"}', done);
   })
 })
