9822440b6c05226656339dbc06455b274bf0c915
Alex B <me@alexb.ninja>
2014-09-14 17:48:18 -0700

    buyan sucks

--- index.js
+++ index.js
@@ -29,7 +29,16 @@ function uhoh(req, res, next) {
   res.send({'404':'content not found'})
 }
 
-var server = restify.createServer();
+var server = restify.createServer({
+  name: 'API'
+});
+
+server.on('after', function (req, res, route, err) {
+    var latency = Date.now() - req.time();
+    console.log('%s %s %s %sms - %s', req.method, req.url, res.statusCode, latency,
+        res.get('Content-Length'));
+});
+
 server.get('/', root);
 server.head('/', root);
 server.get('/hello', nameEmpty);
