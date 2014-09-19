6329b1a8a883d37b6d1efa5501c3a0acae908d48
Alex B <me@alexb.ninja>
2014-09-19 02:20:18 -0700

    moved to express

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-var restify = require('restify');
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var logger = require('morgan');
 var h = require('./handlers');
 var l = require('./laws.js');
 var port = Number(process.env.PORT || 8080);
 
-var server = restify.createServer({
-  name: 'borderly-api'
-});
-server.use(restify.bodyParser());
-server.use(restify.CORS());
-server.use(restify.fullResponse());
+var app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(logger('dev'));
 
-server.get('/', h.root);
-server.head('/', h.root);
-server.get('/laws', l.listLaws);
-server.head('/laws', l.listLaws);
-server.get('/laws/:state', l.lawsByState);
-server.head('/laws/:state', l.lawsByState);
-server.get('/laws/create', l.lawsCreate);
-server.head('/laws/create', l.lawsCreate);
-server.post('/laws/create', l.lawsCreate);
-server.get('/laws/remove/:id', l.lawsRemove);
-server.head('/laws/remove/:id', l.lawsRemove);
-server.get('/(.*)/', h.uhoh);
-server.head('/(.*)/', h.uhoh);
+app.get('/', h.root);
+app.head('/', h.root);
+app.get('/laws', l.listLaws);
+app.head('/laws', l.listLaws);
+app.get('/laws/:state', l.lawsByState);
+app.head('/laws/:state', l.lawsByState);
+app.get('/laws/create', l.lawsCreate);
+app.head('/laws/create', l.lawsCreate);
+app.post('/laws/create', l.lawsCreate);
+app.get('/laws/remove/:id', l.lawsRemove);
+app.head('/laws/remove/:id', l.lawsRemove);
+app.get('/(.*)/', h.uhoh);
+app.head('/(.*)/', h.uhoh);
 
-server.listen(port, '0.0.0.0', function() {
-  console.log('%s listening at %s', server.name, server.url);
+app.listen(port, function(){
+  console.log('server running on port: %d', port);
 });
