1f42aa5321c93a2f643208421538fd59215bdeac
Alex B <me@alexb.ninja>
2014-09-19 14:04:45 -0700

    loader.io verfication

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.head('/laws/create', l.lawsCreate);
 app.post('/laws/create', l.lawsCreate);
 app.get('/laws/remove/:id', l.lawsRemove);
 app.head('/laws/remove/:id', l.lawsRemove);
+app.get('/loaderio-d6800512a9c950194f6210dca6b7405f', function(req, res, next){
+  res.send('loaderio-d6800512a9c950194f6210dca6b7405f')
+  next();
+});
 app.get('/(.*)/', h.uhoh);
 app.head('/(.*)/', h.uhoh);
 
