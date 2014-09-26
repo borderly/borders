e8de8cdb9cec5494c923be7ceeb22977dfae69fb
Alex B <me@alexb.ninja>
2014-09-25 17:49:52 -0700

    fix the 404 page

--- index.js
+++ index.js
@@ -38,17 +38,14 @@ if(app.get('env') === 'development') {
 
 app.use(function(req, res, next){
   res.status(404);
-
   if (req.accepts('html')) {
-    res.render('404', { url: req.url });
+    res.render('404', { url: req.url, title: req.url + ' - Not found' });
     return;
   }
-
   if (req.accepts('json')) {
     res.send({ error: 'Not found' });
     return;
   }
-
   res.type('txt').send('Not found');
 });
 
