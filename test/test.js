54f791463cf2a4131acce0133b9743d0706411cd
Alex B <me@alexb.ninja>
2014-10-01 11:09:25 -0700

    update test order

--- test/test.js
+++ test/test.js
@@ -16,6 +16,21 @@ describe('GET /', function(){
   })
 })
 
+describe('GET /create', function(){
+  it('respond with html', function(done){
+    request(app)
+      .get('/create')
+      .set('Accept', 'text/html')
+      .expect('Content-Type', /html/)
+      .expect(200, done);
+  })
+  it('repsond with 200 status', function(done){
+    request(app)
+      .get('/create')
+      .expect(200, done);
+  })
+})
+
 describe('GET /laws', function(){
   it('respond with json', function(done){
     request(app)
@@ -42,21 +57,6 @@ describe('GET /laws/:name', function(){
   it('repsond with 200 status', function(done){
     request(app)
       .get('/api/laws/ca')
-      .expect(200, done);
-  })
-})
-
-describe('GET /create', function(){
-  it('respond with html', function(done){
-    request(app)
-      .get('/create')
-      .set('Accept', 'text/html')
-      .expect('Content-Type', /html/)
-      .expect(200, done);
-  })
-  it('repsond with 200 status', function(done){
-    request(app)
-      .get('/create')
       .expect(200, done);
   })
 })
