ce35224908d0e0434d099b874bab4023d901732e
Alex B <me@alexb.ninja>
2014-10-02 22:27:01 -0700

    oh, it works now

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'assets')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
-app.use('/api', lawRoutes);
+app.use('/a(pi)?', lawRoutes);
 app.use('/app', appRoutes);
 app.use('', viewRoutes);
 app.set('json spaces', 2);
