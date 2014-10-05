98f36a3fc989486a93845053b27c92edf09a9492
Alex B <me@alexb.ninja>
2014-10-04 21:46:48 -0700

    list * added normalize * redo template data * embed jade data * fix 404

--- routes/app.js
+++ routes/app.js
@@ -4,7 +4,13 @@ var router = express.Router();
 var Law = mongoose.model('Law')
 
 router.get('/', function(req, res, next) {
-  res.render('app/index');
+  Law.find({}).sort({state: 1}).exec(function(err, results){
+    console.log(results)
+    res.render('app/index', {
+      pageTitle: 'Law index',
+      laws: results
+    });
+  })
 });
 
 router.get('/test', function(req, res, next) {
@@ -15,14 +21,14 @@ router.get('/law/:id', function(req, res, next) {
   Law.findById(req.params.id, function(err, doc) {
     res.render('app/law', {
       pageTitle: 'Law listing',
-      _id: doc._id,
-      section: doc.section,
-      title: doc.title,
-      state: doc.state,
-      county: doc.county,
-      law: doc.law
+      law: doc
     })
   });
+});
+
+router.use(function(req, res, next){
+  res.status(404);
+  res.render('404', { url: req.originalUrl, title: req.url + ' - Not found', laws: true});
 });
 
 module.exports = router;
